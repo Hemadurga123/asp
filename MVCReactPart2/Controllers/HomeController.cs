f022cdbddfa2d93ac9eaf2bd55be7293cc45d45b
hema <Hemadurga@zelarsoft.com>
2021-08-27 11:26:02 +0530

    updated code

---
+++ MVCReactPart2/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCReactPart2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getEmployeeList()
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var data = dc.Employees.OrderBy(a => a.FirstName).ToList();
+                return new JsonResult { Data = data,  JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+	}
+}
